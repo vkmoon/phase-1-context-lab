d1b5bc7e5546f50bb1c5c88eea62a395c49d7188
Valeria <valeriakirkbride@gmail.com>
2023-10-06 15:14:10 -0600

    context lab

--- index.js
+++ index.js
@@ -1,4 +1,72 @@
-/* Your Code Here */
+function createEmployeeRecord(employeeAttributes) {
+    let employeeRecord = {};
+    employeeRecord["firstName"] = employeeAttributes[0]
+    employeeRecord["familyName"] = employeeAttributes[1]
+    employeeRecord["title"] = employeeAttributes[2]
+    employeeRecord["payPerHour"] = employeeAttributes[3]
+    employeeRecord["timeInEvents"] = []
+    employeeRecord["timeOutEvents"] = []
+    return employeeRecord;
+}
+
+function createEmployeeRecords(arrays) {
+    return arrays.map(array => createEmployeeRecord(array));
+}
+
+function createTimeInEvent(timeIn) {
+    let dateTime = timeIn.split(" ");
+    this["timeInEvents"].push({
+    "type": "TimeIn",
+    "hour": parseInt(dateTime[1]),
+    "date": dateTime[0]
+    });
+    return this;
+}
+
+function createTimeOutEvent(timeOut) {
+    let dateTime = timeOut.split(" ");
+    this["timeOutEvents"].push({
+    "type": "TimeOut",
+    "hour": parseInt(dateTime[1]),
+    "date": dateTime[0]
+    });
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    let timeInEvent = this.timeInEvents.find(event => event.date === date);
+    let timeOutEvent = this.timeOutEvents.find(event => event.date === date);
+
+    if (timeInEvent && timeOutEvent) {
+        let timeInMinutes = parseInt(timeInEvent.hour);
+        let timeOutMinutes = parseInt(timeOutEvent.hour);
+        let hoursWorked = (timeOutMinutes - timeInMinutes) / 100;
+        return hoursWorked;
+    }
+    {
+        return 0
+    }
+}
+
+function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour
+}
+
+function findEmployeeByFirstName(collection, firstNameString) {
+    for (let employee of collection) {
+      if (employee.firstName === firstNameString) {
+        return employee;
+      }
+    }
+    return undefined;
+  }
+
+  function calculatePayroll(employeeRecords) { 
+    return employeeRecords.reduce((totalPayRoll, employee) => {
+        let employeeWages = allWagesFor.call(employee); 
+        return totalPayRoll + employeeWages;
+    }, 0);
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
